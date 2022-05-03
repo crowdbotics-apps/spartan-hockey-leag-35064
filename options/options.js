1255c42b70bcce53a1ffe1d1b09cbcfb5f4e4f40
Crowdbotics <team@crowdbotics.com>
2022-05-03 19:05:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "spartan_hockey_leag_35064",
+  url: "https://spartan_hockey_leag_35064.botics.co",
+  api: "https://spartan_hockey_leag_35064.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
